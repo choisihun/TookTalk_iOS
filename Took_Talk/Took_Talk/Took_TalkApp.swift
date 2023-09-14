4306fdaf46d50cd626db716741a06590d370ab29
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-09-14 23:51:48 +0900

    🎉 :: FirstCommit

---
+++ Took_Talk/Took_Talk/Took_TalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Took_TalkApp.swift
+//  Took_Talk
+//
+//  Created by 최시훈 on 2023/09/14.
+//
+
+import SwiftUI
+
+@main
+struct Took_TalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
