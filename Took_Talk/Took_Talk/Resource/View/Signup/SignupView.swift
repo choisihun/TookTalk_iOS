0e370173b4829f4a7ec128827080791b0d8c5d17
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2023-11-22 20:57:38 +0900

    ✨ :: Signup UI 구현 완료

--- Took_Talk/Took_Talk/Resource/View/Signup/SignupView.swift
+++ Took_Talk/Took_Talk/Resource/View/Signup/SignupView.swift
@@ -13,23 +13,33 @@ struct SignupView: View {
     
     @ObservedObject var state = SignupState()
     var body: some View {
-        ZStack {
-            Image("backgroundLogo")
-                .padding(.horizontal, 0)
-                .padding(.bottom, 40)
-                .frame(maxHeight: .infinity, alignment: .bottom)
-            VStack {
-                SignupTextFieldsView()
-                    .padding(.top, 30)
-                    .frame(maxHeight: .infinity,alignment: .top)
+        NavigationView {
+            ZStack {
+                Image("backgroundLogo")
                     .padding(.horizontal, 0)
+                    .padding(.bottom, 40)
+                    .frame(maxHeight: .infinity, alignment: .bottom)
+                VStack {
+                    SignupTextFieldsView()
+                        .padding(.bottom, 100)
+                        .frame(maxHeight: .infinity,alignment: .top)
+                        .padding(.horizontal, 0)
+                }
+                .padding(.vertical)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
             }
-            .padding(.vertical)
-            .frame(alignment: .center)
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+            .padding(.bottom, 50)
+            
         }
+        .navigationBarItems(leading: Button {
+            state.back()
+        }
+        label: {
+            Text("\(Image(systemName: "chevron.backward"))뒤로가기")
+        })
     }
 }
-
 
 struct SignupView_Previews: PreviewProvider {
     static var previews: some View {
