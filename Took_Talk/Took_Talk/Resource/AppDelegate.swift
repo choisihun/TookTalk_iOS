366b89b786e51bb4f456da87bc674668835666df
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-03-05 08:55:33 +0900

    Create AppDelegate.swift

---
+++ Took_Talk/Took_Talk/Resource/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Took_Talk
+//
+//  Created by 최시훈 on 11/22/23.
+//
+
+import SwiftUI
+import FirebaseCore
+
+final class AppDelegate: NSObject {
+    
+}
+
+// MARK: UIApplicationDelegate
+
+extension AppDelegate: UIApplicationDelegate {
+  func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+      FirebaseApp.configure()
+    return true
+  }
+}
