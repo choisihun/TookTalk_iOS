c222d63e5e67755c448a036f2ea42b85f7ec7b20
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-03-06 14:13:58 +0900

    ✨ :: Create CustomSelectAgeView

---
+++ Took_Talk/Took_Talk/Resource/View/Home/CustomAge/CustomSelectAgeView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomSelectAgeView.swift
+//  Took_Talk
+//
+//  Created by 최시훈 on 11/20/23.
+//
+
+import SwiftUI
+
+struct CustomSelectAgeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    CustomSelectAgeView()
+}
