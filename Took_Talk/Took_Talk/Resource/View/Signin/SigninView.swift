7c16afca3f0a10b4b4dc72f0c8ad76779ddbfa38
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-03-07 21:28:22 +0900

    ✨ :: SigninView UI구현

--- Took_Talk/Took_Talk/Resource/View/Signin/SigninView.swift
+++ Took_Talk/Took_Talk/Resource/View/Signin/SigninView.swift
@@ -12,38 +12,53 @@ struct SigninView: View {
     @Environment(\.dismiss) private var dismiss
     
     
-    @StateObject private var state = SigninState()
+    @StateObject private var viewModel = SigninViewModel()
     
     
     var body: some View {
         NavigationView {
             VStack {
-                Image("mainLogo")
-                    .padding(.top, 50)
+                Image("TookTalkMainLogo")
+                    .padding(.top, 46)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                Text("또래와의 소통이 더욱 쉬워지는\nTook Talk으로 지금 시작해보세요!")
+                    .foregroundColor(.gray)
+                    .multilineTextAlignment(.center)
+                    .font(.custom(pretendardRegular, size: 12))
                     .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
                 
                 VStack {
-                    TextField("아이디를 입력해주세요", text: $state.id)
-                        .frame(width: 290, height: 50)
+                    HStack {
+                        Text("아이디")
+                            .font(.custom(pretendardSemiBold, size: 13))
+                        
+                        Spacer()
+                    }
+                    .padding(.top, 30)
+                    .padding(.leading, 5)
+                    .padding(.bottom, 0)
                     
-                    Rectangle()
-                        .frame(width: 290, height: 0.7)
-                        .foregroundColor(Color("myGray"))
-                        .padding(.top, 0)
+                    CustomSignTextField(placeholder: "아이디를 입력해주세요", textfieldValue: viewModel.id)
                     
-                    SecureField("비밀번호를 입력해주세요", text: $state.password)
-                        .frame(width: 290, height: 50)
-                        .padding(.top)
+                    HStack {
+                        Text("비밀번호")
+                            .font(.custom(pretendardSemiBold, size: 13))
+                        
+                        Spacer()
+                    }
+                    .padding(.top, 30)
+                    .padding(.leading, 5)
+                    .padding(.bottom, 0)
                     
-                    Rectangle()
-                        .frame(width: 290, height: 0.7)
+                    CustomSignSecureField(placeholder: "비밀번호를 입력해주세요", textfieldValue: viewModel.password)
                         
                     HStack {
+                        Spacer()
                         Button(action: {
                             
                         }) {
                             Text("비밀번호 찾기")
-                                .font(.custom(pretendardLight, size: 15))
+                                .font(.custom(pretendardThin, size: 13))
                                 .foregroundColor(Color.black)
                                 .fontWeight(.semibold)
                         }
@@ -52,22 +67,24 @@ struct SigninView: View {
                             .frame(width: 1, height: 20)
                             .foregroundColor(Color("myGray"))
                         Button {
-                            state.signup()
+                            viewModel.signup()
                         } label: {
                             Text("회원가입")
-                                .font(.custom(pretendardLight, size: 15))
-                                .frame(width: 55, height: 18)
+                                .font(.custom(pretendardThin, size: 13))
                                 .foregroundColor(Color.black)
                                 .fontWeight(.semibold)
                         }
-                        
                     }
-                    .padding(.trailing, 50)
+                    .padding(.trailing, 15)
                     .frame(maxWidth: .infinity, alignment: .trailing)
+                    .padding(.bottom, 12)
+                    
+                    Spacer()
+                    
                     Group {
                             NavigationLink(
                                 destination: HomeView(),
-                                isActive: $state.isWaitingViewActive
+                                isActive: $viewModel.isWaitingViewActive
                                 
                             ) {
                                 EmptyView()
@@ -75,30 +92,28 @@ struct SigninView: View {
                             }
                             .hidden()
                             Button(action: {
-                                state.login()
+                                viewModel.login()
                             }) {
                                 Text("로그인")
-                                    .font(.custom(pretendardMedium, size: 30))
-                                    .frame(width: 290, height: 50)
+                                    .font(.custom(pretendardRegular, size: 20))
+                                    .frame(width: 320, height: 50)
                             }
                         
                     }
                     .background(Color("myOrange"))
                     .foregroundColor(Color.white)
                     .cornerRadius(15)
-                    .padding(.top, 33)
+                    .padding(.bottom, 87)
                 }
+                .padding(.horizontal, 45)
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-                Image("backgroundLogo")
                 
+                Spacer()
             }
         }
     }
 }
 
-
-struct SigninView_Previews: PreviewProvider {
-    static var previews: some View {
-        SigninView()
-    }
+#Preview {
+    SigninView()
 }
