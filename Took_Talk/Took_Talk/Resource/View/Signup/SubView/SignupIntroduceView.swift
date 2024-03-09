b18f515e4313a45861797ce61501fac614e55359
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-03-10 03:09:13 +0900

    Create SignupIntroduceView.swift

---
+++ Took_Talk/Took_Talk/Resource/View/Signup/SubView/SignupIntroduceView.swift
@@ -0,0 +1,18 @@
+//
+//  SignupIntroduceView.swift
+//  Took_Talk
+//
+//  Created by 최시훈 on 3/10/24.
+//
+
+import SwiftUI
+
+struct SignupIntroduceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignupIntroduceView()
+}
