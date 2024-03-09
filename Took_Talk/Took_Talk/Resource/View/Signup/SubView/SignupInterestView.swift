3d62d5964e1ede24642c0d16478feaafd60e2733
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-03-10 03:09:10 +0900

    Create SignupInterestView.swift

---
+++ Took_Talk/Took_Talk/Resource/View/Signup/SubView/SignupInterestView.swift
@@ -0,0 +1,18 @@
+//
+//  SignupInterestView.swift
+//  Took_Talk
+//
+//  Created by 최시훈 on 3/10/24.
+//
+
+import SwiftUI
+
+struct SignupInterestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignupInterestView()
+}
