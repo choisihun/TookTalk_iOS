6f033949add3d711d0033b8431b731040689313a
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-03-11 14:22:05 +0900

    🎆 :: Signup Logo Image 최종본

--- Took_Talk/Took_Talk/Resource/View/Signup/SubView/SignupPhoneNumberView.swift
+++ Took_Talk/Took_Talk/Resource/View/Signup/SubView/SignupPhoneNumberView.swift
@@ -11,17 +11,48 @@ struct SignupPhoneNumberView: View {
     
     @StateObject var viewModel = SignupPhoneNumberViewModel()
     var body: some View {
-        VStack {
-            CustomSignTextField(placeholder: "전화번호를 입력해주세요", textfieldValue: viewModel.phoneNumber)
+        VStack(alignment: .leading) {
+            HStack {
+                Text("전화번호")
+                    .font(.custom(pretendardRegular, size: 15))
+                
+                Spacer()
+            }
+            .padding(.bottom, 0)
+
+            HStack {
+                
+                TextField("전화번호를 입력해주세요", text: $viewModel.phoneNumber)
+                Button {
+                    
+                } label: {
+                    Text("인증 요청")
+                        .font(.custom(pretendardRegular, size: 15))
+                }
+                .buttonStyle(CustomStrokedButtonStyle(foregroundColor: .black, borderColor: Color("myOrange"), radius: 10))
+            }
+            .padding(.top, 0)
+            .frame(width: 290, height: 28)
+
+            Rectangle()
+                .foregroundColor(Color("myGray"))
+                .frame(width: 210, height: 0.7, alignment: .topLeading)
+                .padding(.top, 0)
+                .padding(.bottom, 30)
+
+
+                
             
-            CustomSignTextField(placeholder: "전화번호를 입력해주세요", textfieldValue: viewModel.certificationNumber)
+            
+            CustomSignTextField(text: "인증번호", placeholder: "인증번호를 입력해주세요", textfieldValue: viewModel.certificationNumber)
+            
+//            Spacer()
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
-        .padding(.bottom, 50)
-        
+        .padding(.top, 0)
     }
 }
 
 #Preview {
     SignupPhoneNumberView()
 }
+
