01773f16270891d56813d63867930bc36d9883ee
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-03-11 14:27:19 +0900

    ✨ :: UI Update, add explanation

--- Took_Talk/Took_Talk/Resource/View/CustomSignTextField.swift
+++ Took_Talk/Took_Talk/Resource/View/CustomSignTextField.swift
@@ -8,18 +8,31 @@
 import SwiftUI
 
 struct CustomSignTextField: View/*, TextFieldRepresentable*/ {
-     @State var placeholder: String
-     @State var textfieldValue: String
+    @State var text: String?
+    @State var placeholder: String
+    @State var textfieldValue: String
+    
     var body: some View {
         VStack {
+            if text != nil {
+                HStack {
+                    Text(text ?? "")
+                        .font(.custom(pretendardRegular, size: 15))
+                    
+                    Spacer()
+                }
+                .padding(.bottom, 0)
+            }
             TextField(placeholder, text: $textfieldValue)
-                .frame(width: 290, height: 35)
+                .frame(width: 290, height: 28)
             
             Rectangle()
                 .frame(width: 290, height: 0.7)
                 .foregroundColor(Color("myGray"))
                 .padding(.top, 0)
         }
+        .frame(width: 290, alignment: .center)
+
     }
 }
 
