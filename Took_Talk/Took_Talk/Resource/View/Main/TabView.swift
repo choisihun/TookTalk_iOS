47e68546030c57cd5815d1a4322c5d8bab4825c3
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-03-13 11:38:59 +0900

    change : Change group

---
+++ Took_Talk/Took_Talk/Resource/View/Main/TabView.swift
@@ -0,0 +1,20 @@
+//
+//  TabView.swift
+//  Took_Talk
+//
+//  Created by 최시훈 on 3/13/24.
+//
+
+import SwiftUI
+
+struct TabView: View {
+    var body: some View {
+        TabView() {
+            
+        }  
+    }
+}
+
+#Preview {
+    TabView()
+}
