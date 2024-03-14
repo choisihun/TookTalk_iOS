abf63a175faf979aec5b51570ad530cba20fec82
DGSWcsh <101883606+ChoiSihun@users.noreply.github.com>
2024-03-14 12:03:23 +0900

    ✨ :: HomeView 리펙토링

--- Took_Talk/Took_Talk/Resource/View/Home/HomeView.swift
+++ Took_Talk/Took_Talk/Resource/View/Home/HomeView.swift
@@ -11,72 +11,78 @@ struct HomeView: View {
     @State var setAge: Int = 15
     @State var isWaitingViewActive: Bool = false
     @State private var isCustomBackButtonHidden = true
-    @StateObject private var state = HomeState()
+    @StateObject var viewModel = HomeViewModel()
     
     var body: some View {
         NavigationView {
             VStack(spacing: 0 ) {
-                Group {
-                    HStack {
-                        Text("관심사")
-                            .frame(alignment: .leading)
-                            .foregroundStyle(.black)
-                            .font(.custom(pretendardSemiBold, size: 19))
-                            .padding(.bottom, 10)
-                            .padding(.leading, 30)
-                        Text("1가지는 필수로 골라주세요!")
-                            .foregroundStyle(.gray)
-                            .padding(.leading, 5)
-                            .font(.custom(pretendardMedium, size: 13))
-                        Spacer()
-                    }
-                    //                    .padding(.top, 50)
-                }
+                Image("TookTalkMainLogo")
+                    .padding(.top, 46)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
                 
-                SelectInterestView()
+                Text("상대의 MBTI")
+                    .foregroundStyle(.gray)
+                    .frame(width: 200, height: 50, alignment: .top)
+                    .font(.custom(pretendardRegular, size: 20))
+                MbtiPickerView(selected: $viewModel.mbti)
+                    .frame(width: 79, height: 39)
+                    .padding(.vertical, 5)
+                    .padding(.horizontal, 10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(Color.blue, lineWidth: 1)
+                    )
                 
-                Group {
-                    Text("-만나고 싶은 상대의 나이-")
-                        .foregroundStyle(.gray)
-                        .frame(width: 200, height: 50, alignment: .center)
-                        .font(.custom(pretendardMedium, size: 15))
-                    
-                    AgePickerView(values: Array(14...19), selected: $setAge)
-                        .foregroundStyle(.black)
-                        .frame(width: 100, height: 50, alignment: .center)
-                        .padding(.top, -45)
-                }
-                .padding(.bottom, 20)
+                Text("상대의 나이")
+                    .foregroundStyle(.gray)
+                    .frame(width: 200, height: 50, alignment: .center)
+                    .font(.custom(pretendardRegular, size: 20))
                 
-                Group {
-                    Text("-만나고 싶은 상대의 성별-")
-                        .foregroundStyle(.gray)
-                        .font(.custom(pretendardMedium, size: 15))
-                        .frame(width: 200, height: 50, alignment: .center)
-                        .padding(.top, -30)
-                    SexSwitchView()
-                        .frame(width: 275, height: 120)
-                        .padding(.horizontal, 50)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 30)
-                                .stroke(Color.gray, lineWidth: 0.3)
-                                .padding(.horizontal, 65)
-                                .padding(.vertical, -10)
-                            
-                        )
-                }
-                .padding(.bottom, 30)
+                AgePickerView(values: Array(14...19), selected: $setAge)
+                    .foregroundStyle(.black)
+                    .frame(width: 76, height: 50, alignment: .center)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 16)
+                            .stroke(Color.blue, lineWidth: 1)
+                    )
+                    .padding(.vertical, 5)
+                    .padding(.horizontal, 10)
+                
+                
+                //                MbtiPickerView(selected: $viewModel.mbti)
+                //                    .frame()
+                //                    .background(.red)
+                
+                Text("상대의 성별")
+                    .foregroundStyle(.gray)
+                    .font(.custom(pretendardRegular, size: 20))
+                    .frame(width: 200, height: 50, alignment: .center)
+                SexSwitchView()
+                    .frame(width: 275, height: 120)
+                    .padding(.horizontal, 50)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 30)
+                            .stroke(Color.gray, lineWidth: 0.3)
+                            .padding(.horizontal, 65)
+                            .padding(.vertical, -10)
+                        
+                    )
+                
+                    .padding(.bottom, 30)
                 Button {
-                    state.routeToWaitingView()
+                    viewModel.routeToWaitingView()
                 } label: {
-                    Text("\(Image(systemName: "arrow.forward"))")
-                        .font(.title)
-                        .foregroundStyle(.black)
+                    Text("START")
+                        .font(.custom(pretendardBold, size: 20))
+                        .foregroundColor(.white)
                 }
-                .frame(width: 120, height: 120, alignment: .center)
+                .frame(width: 137, height: 45, alignment: .center)
                 .background(Color("myOrange"))
-                .cornerRadius(30)
+                .cornerRadius(10)
+                
+                Spacer()
             }
+            
         }
         .toolbar(.visible, for: .navigationBar)
         .navigationBarBackButtonHidden(true)
@@ -89,7 +95,7 @@ struct HomeView: View {
                     .frame(width: 20, height: 20)
                     .foregroundColor(.black)
                     .font(.system(size: 20))
-
+                
             }
         })
     }
